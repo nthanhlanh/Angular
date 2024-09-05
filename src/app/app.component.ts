f8e78717a059a83ac78143ea8d04248e238e283a
lanh <nguyenthanhlanh91@gmail.com>
2024-09-05 20:54:59 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from './store/counter.actions';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-angular-app';
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    // Gán giá trị cho count$ trong constructor
+    this.count$ = this.store.select('count');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideStore } from '@ngrx/store';
+import { counterReducer } from './app/store/counter.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';  // Import Devtools
+import { importProvidersFrom } from '@angular/core'; // Import provider utility
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideStore({ count: counterReducer }),
+  ]
+}).catch(err => console.error(err));
